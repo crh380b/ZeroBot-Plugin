4b2bb43eb69ef4ebdcd2840f3c1caa2b1fe88591
crh380b <122514018+crh380b@users.noreply.github.com>
2023-01-12 15:43:46 +0800

    rename

--- main.go
+++ main.go
@@ -237,7 +237,7 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
+	// sus = append(sus, 3556227684)
 	// sus = append(sus, 87654321)
 
 	if *runcfg != "" {
@@ -264,7 +264,7 @@ func init() {
 
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:       append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
+		NickName:       append([]string{*adana}, "带鱼"),
 		CommandPrefix:  *prefix,
 		SuperUsers:     sus,
 		RingLen:        *rsz,
